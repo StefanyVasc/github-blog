e340d41b0ba32ba5c87509e16d57a7b9a23e5d67
Stefany Sá <stefanysa@MacBook-Pro-de-Stefany.local>
2024-01-13 15:24:57 -0300

    chore: add styles for scroll

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,6 +6,19 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     outline: none;
+
+    ::-webkit-scrollbar {
+      width: 0.45rem;
+    }
+
+    ::-webkit-scrollbar-track {
+      background: ${({ theme }) => theme.colors['base-border']}
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background: ${({ theme }) => theme.colors['brand-blue']};
+      border-radius: 999px;
+    }
   }
 
   body {
