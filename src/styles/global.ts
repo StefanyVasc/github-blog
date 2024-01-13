8461c2e31ad3f08218109c1b9cad730e9ad992f5
Stefany Sá <stefanysa@MacBook-Pro-de-Stefany.local>
2024-01-13 15:12:53 -0300

    chore: add styled-components and configs

---
+++ src/styles/global.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    background: ${({ theme }) => theme.colors['base-background']};
+    color: ${({ theme }) => theme.colors['base-text']};
+  }
+  
+  body, input, textarea, button {
+    font: 400 ${({ theme }) =>
+      theme.textSizes['text-text-m']} 'Nunito', sans-serif;
+      line-height: 160%;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
