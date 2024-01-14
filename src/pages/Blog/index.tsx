36cee5793827e0095fa98c9433ed5d3b48f0654b
StefanyVasc <stefany.vasc.sa@gmail.com>
2024-01-14 19:19:53 -0300

    feat(Blog): add logic in Blog

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,16 +1,60 @@
+import { useCallback, useEffect, useState } from 'react'
 import { Post } from './components/Post'
 import { Profile } from './components/Profile'
 import { SearchInput } from './components/SearchInput'
 import * as S from './styles'
+import { api } from '../../lib/axios'
+import { Spinner } from '../../components/Spinner'
+
+const username = import.meta.env.VITE_GITHUB_USERNAME
+const reponame = import.meta.env.VITE_GITHUB_REPONAME
+
+export interface PostType {
+  title: string
+  body: string
+  created_at: string
+  number: number
+  html_url: string
+  labels_url: string
+  comments: string
+  url: {
+    login: string
+  }
+}
 
 export function Blog() {
+  const [posts, setPosts] = useState<PostType[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getPosts = useCallback(async (query: string = '') => {
+    try {
+      setIsLoading(true)
+      const response = await api.get(
+        `/search/issues?q=${query}%20label:published%20repo:${username}/${reponame}`,
+      )
+      setPosts(response.data.items)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    getPosts()
+  }, [getPosts])
+
   return (
-    <div>
+    <>
       <Profile />
-      <SearchInput />
-      <S.PostsListContainer>
-        <Post />
-      </S.PostsListContainer>
-    </div>
+      <SearchInput postsLength={posts.length} getPosts={getPosts} />
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <S.PostsListContainer>
+          {posts.map((post) => (
+            <Post key={post.number} post={post} />
+          ))}
+        </S.PostsListContainer>
+      )}
+    </>
   )
 }
