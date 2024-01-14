88e1f901cb244de7db3661dc6eca8da382c2a873
StefanyVasc <stefany.vasc.sa@gmail.com>
2024-01-14 19:19:09 -0300

    feat(Blog): add form validation at Search\Input

--- src/pages/Blog/components/SearchInput/index.tsx
+++ src/pages/Blog/components/SearchInput/index.tsx
@@ -1,13 +1,37 @@
+import * as z from 'zod'
 import * as S from './styles'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-export function SearchInput() {
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInput = z.infer<typeof searchFormSchema>
+
+interface SearchInputProps {
+  postsLength: number
+  getPosts: (query?: string) => Promise<void>
+}
+
+export function SearchInput({ getPosts, postsLength }: SearchInputProps) {
+  const { register, handleSubmit } = useForm<SearchFormInput>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchPosts(data: SearchFormInput) {
+    await getPosts(data.query)
+  }
+
   return (
-    <S.SearchInputContainer>
+    <S.SearchInputContainer onSubmit={handleSubmit(handleSearchPosts)}>
       <header>
         <h3>Publicações</h3>
-        <span>6 publicações</span>
+        <span>
+          {postsLength} {postsLength > 1 ? 'publicações' : 'publicação'}
+        </span>
       </header>
-      <input type="text" placeholder="Buscar conteúdo" />
+      <input type="text" placeholder="Buscar conteúdo" {...register('query')} />
     </S.SearchInputContainer>
   )
 }
