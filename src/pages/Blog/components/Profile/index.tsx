fe046b5713f0d4ca81b6981da8501eb6f1e04739
StefanyVasc <stefany.vasc.sa@gmail.com>
2024-01-14 19:19:35 -0300

    feat(Blog): add logic in Profile

--- src/pages/Blog/components/Profile/index.tsx
+++ src/pages/Blog/components/Profile/index.tsx
@@ -3,32 +3,79 @@ import { faBuilding, faUserGroup } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { ExternalLink } from '../../../../components/ExternalLink'
 import * as S from './styles'
+import { useCallback, useEffect, useState } from 'react'
+import { api } from '../../../../lib/axios'
+import { Spinner } from '../../../../components/Spinner'
+
+const username = import.meta.env.VITE_GITHUB_USERNAME
+
+interface ProfileData {
+  login: string
+  bio: string
+  avatar_url: string
+  html_url: string
+  name: string
+  company?: string
+  followers: number
+}
 
 export function Profile() {
+  const [profileData, setProfileData] = useState<ProfileData>({} as ProfileData)
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getProfileData = useCallback(async () => {
+    try {
+      setIsLoading(true)
+      const response = await api.get(`/users/${username}`)
+
+      setProfileData(response.data)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    getProfileData()
+  }, [getProfileData])
+
   return (
     <S.ProfileContainer>
-      <S.ProfilePicture src="https://avatars.githubusercontent.com/u/39599492?v=4" />
-      <S.ProfileDetails>
-        <header>
-          <h1>Stefany Sá</h1>
-          <ExternalLink text="Github" href="#" />
-        </header>
-        <p>Frontend & UI Developer.</p>
-        <ul>
-          <li>
-            <FontAwesomeIcon icon={faGithub} />
-            StefanyVasc
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faBuilding} />
-            Enterprise
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faUserGroup} />
-            100 seguidores
-          </li>
-        </ul>
-      </S.ProfileDetails>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <S.ProfilePicture src={profileData.avatar_url} />
+
+          <S.ProfileDetails>
+            <header>
+              <h1>{profileData.name}</h1>
+
+              <ExternalLink
+                text="Github"
+                href={profileData.html_url}
+                target="_blank"
+              />
+            </header>
+            <p>{profileData.bio}</p>
+            <ul>
+              <li>
+                <FontAwesomeIcon icon={faGithub} />
+                {profileData.login}
+              </li>
+              {profileData?.company && (
+                <li>
+                  <FontAwesomeIcon icon={faBuilding} />
+                  {profileData.company}
+                </li>
+              )}
+              <li>
+                <FontAwesomeIcon icon={faUserGroup} />
+                {profileData.followers} seguidores
+              </li>
+            </ul>
+          </S.ProfileDetails>
+        </>
+      )}
     </S.ProfileContainer>
   )
 }
