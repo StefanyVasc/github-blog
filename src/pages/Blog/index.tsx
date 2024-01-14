0d82b99d6c4296e3a80b360c19389359ba1fa413
StefanyVasc <stefany.vasc.sa@gmail.com>
2024-01-14 15:35:49 -0300

    chore: use Post component

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,11 +1,16 @@
+import { Post } from './components/Post'
 import { Profile } from './components/Profile'
 import { SearchInput } from './components/SearchInput'
+import * as S from './styles'
 
 export function Blog() {
   return (
     <div>
       <Profile />
       <SearchInput />
+      <S.PostsListContainer>
+        <Post />
+      </S.PostsListContainer>
     </div>
   )
 }
