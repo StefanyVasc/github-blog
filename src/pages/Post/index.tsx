26f90168f09555b9b30c8b3d7e627ac4234ba84c
StefanyVasc <stefany.vasc.sa@gmail.com>
2024-01-14 21:31:14 -0300

    feat: crate logic for Post page

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,9 +1,42 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { api } from '../../lib/axios'
+import { PostType } from '../Blog'
 import { PostHeader } from './components/PostHeader'
+import { PostContent } from './components/PostContent'
+
+const username = import.meta.env.VITE_GITHUB_USERNAME
+const reponame = import.meta.env.VITE_GITHUB_REPONAME
 
 export function Post() {
+  const [postData, setPostData] = useState<PostType>({} as PostType)
+  const [isLoading, setIsLoading] = useState(true)
+
+  const { id } = useParams()
+
+  const getPostDetails = useCallback(async () => {
+    try {
+      setIsLoading(true)
+
+      const response = await api.get(
+        `/repos/${username}/${reponame}/issues/${id}`,
+      )
+      console.log(response)
+      setPostData(response.data)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [id])
+
+  useEffect(() => {
+    getPostDetails()
+  }, [getPostDetails])
+
   return (
     <>
-      <PostHeader />
+      {console.log(postData)}
+      <PostHeader isLoading={isLoading} postData={postData} />
+      {!isLoading && <PostContent content={postData.body} />}
     </>
   )
 }
